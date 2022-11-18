057179743b933d73c18b126083274c18b331d74f
Mateus Ladika <89612599+MateusLDK@users.noreply.github.com>
2022-11-18 17:36:32 -0300

    Add files via upload
    Adicionando primeiro Bot: concatenador de planilhas em CSV da Bravo.

---
+++ concatenadorBravo.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import os
+import glob
+from tkinter import filedialog as fd
+import time
+
+finalDF = pd.DataFrame()
+colunasValidas = ["NUMERO CTRC", "NUMERO CT-E", "PLACA DE COLETA", "CLIENTE REMETENTE",	"CLIENTE DESTINATARIO",	"ENDERECO RECEBEDOR", 
+                    "CIDADE ENTREGA", "UF ENTREGA", "CEP ENTREGA", "NOTA FISCAL", "FRETE PESO", "OUTROS", "VAL RECEBER", 
+                    "NUMERO DA FATURA", "DATA ENTREGA"]
+
+filePath = fd.askdirectory()
+timeStart = time.time()
+os.chdir(filePath)
+localArquivos = os.getcwd()
+arquivosCSV = glob.glob(os.path.join(localArquivos, "*.csv"))
+
+for f in arquivosCSV:
+      
+    tempDF = pd.read_csv(f, encoding = "ISO-8859-1", skiprows=[0], skipfooter=1, sep=';', usecols=colunasValidas, engine='python')
+    finalDF = pd.concat([finalDF, tempDF], ignore_index=False)
+
+finalDF.to_csv('final/PlanilhaTotal.csv', encoding = "ISO-8859-1", sep=';', index=False, header=True)
+timeEnd = time.time()
+
+print("concluído!")
+
+print("tempo de execução: ",end='')
+print(timeEnd-timeStart)
+print("linhas finalizadas: ",end='')
+print(len(finalDF))
