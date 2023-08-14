4f37e7263f32aa44498b01123898f55df32bba7e
MateusLDK <ladikamateus@gmail.com>
2023-08-14 16:04:41 -0300

    Atualizando códigos

---
+++ lerImagem.py
@@ -0,0 +1,63 @@
+import pytesseract
+import re
+import os.path
+import glob
+import pandas as pd
+from PIL import Image
+from pdf2image import convert_from_path
+from tkinter import filedialog as fd
+
+listaNotas = {}
+arquivosPDF = {}
+popplerPath = "C:/Users/mladika/Documents/Python/Ladika/poppler-23.08.0/Library/Bin"
+tesseractPath = r"C:\Users\mladika\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+
+filePath = fd.askdirectory()
+os.chdir(filePath)
+localArquivos = os.getcwd()
+arquivosPDF = glob.glob(os.path.join(localArquivos, "*.pdf"))
+
+def lerArquivos():
+
+    i = 1
+    for arquivo in arquivosPDF:
+
+        pages = convert_from_path(arquivo, 350, poppler_path=popplerPath)
+
+        for page in pages:
+            image_name = "NF" + str(i) + ".jpg"
+            page.save(image_name, "JPEG")
+            print("Nota " + str(i) + " convertida com sucesso")
+            i+=1
+
+def processarArquivos():
+
+    i=1
+    print(len(arquivosPDF))
+    while i <= len(arquivosPDF):
+
+        img = Image.open(r"C:\Users\mladika\Documents\Python\Ladika\NFs\NF" + str(i) + ".jpg")
+        pytesseract.pytesseract.tesseract_cmd = tesseractPath
+        text = pytesseract.image_to_string(img)
+
+        numeroCTRC = re.findall(r'n°. (\d{4})', text)
+        #numeroCTRC = stringCTRC[0].split(' ')
+        numeroNF = re.findall(r'CURITIBA (\d{4})', text)
+        #numeroNF = stringNF[0].split(' ')
+
+        if numeroCTRC and numeroNF:
+            listaNotas[numeroCTRC[0]] = numeroNF[0]
+            print("Nota " + str(i) + " processada com sucesso")
+            i+=1
+        else:
+            print("algo deu errado aí, meu patrau")
+
+
+lerArquivos()
+
+processarArquivos()
+
+dataFrame = pd.DataFrame(data=listaNotas, index=[0])
+dataFrame = (dataFrame.T)
+print(dataFrame)
+dataFrame.to_excel('teste.xlsx')
