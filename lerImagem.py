3f11854f99d031663488ea65ba0622009a77e488
MateusLDK <ladikamateus@gmail.com>
2023-08-14 17:16:28 -0300

    adicionado multithreading

--- lerImagem.py
+++ lerImagem.py
@@ -3,19 +3,13 @@ import re
 import os.path
 import glob
 import pandas as pd
+import threading
+
+from time import sleep
 from PIL import Image
 from pdf2image import convert_from_path
 from tkinter import filedialog as fd
 
-listaNotas = {}
-arquivosPDF = {}
-popplerPath = "C:/Users/mladika/Documents/Python/Ladika/poppler-23.08.0/Library/Bin"
-tesseractPath = r"C:\Users\mladika\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
-
-filePath = fd.askdirectory()
-os.chdir(filePath)
-localArquivos = os.getcwd()
-arquivosPDF = glob.glob(os.path.join(localArquivos, "*.pdf"))
 
 def lerArquivos():
 
@@ -32,18 +26,17 @@ def lerArquivos():
 
 def processarArquivos():
 
+    
     i=1
     print(len(arquivosPDF))
     while i <= len(arquivosPDF):
-
+        sleep(1)
         img = Image.open(r"C:\Users\mladika\Documents\Python\Ladika\NFs\NF" + str(i) + ".jpg")
         pytesseract.pytesseract.tesseract_cmd = tesseractPath
         text = pytesseract.image_to_string(img)
 
         numeroCTRC = re.findall(r'n°. (\d{4})', text)
-        #numeroCTRC = stringCTRC[0].split(' ')
         numeroNF = re.findall(r'CURITIBA (\d{4})', text)
-        #numeroNF = stringNF[0].split(' ')
 
         if numeroCTRC and numeroNF:
             listaNotas[numeroCTRC[0]] = numeroNF[0]
@@ -52,12 +45,29 @@ def processarArquivos():
         else:
             print("algo deu errado aí, meu patrau")
 
+if __name__ == "__main__":
 
-lerArquivos()
+    listaNotas = {}
+    arquivosPDF = {}
+    popplerPath = "C:/Users/mladika/Documents/Python/Ladika/poppler/Library/Bin"
+    tesseractPath = r"C:\Users\mladika\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
 
-processarArquivos()
+    filePath = fd.askdirectory()
+    os.chdir(filePath)
+    localArquivos = os.getcwd()
+    arquivosPDF = glob.glob(os.path.join(localArquivos, "*.pdf"))
 
-dataFrame = pd.DataFrame(data=listaNotas, index=[0])
-dataFrame = (dataFrame.T)
-print(dataFrame)
-dataFrame.to_excel('teste.xlsx')
+    thread1 = threading.Thread(target = lerArquivos)
+    thread2 = threading.Thread(target = processarArquivos)
+
+    thread1.start()
+    sleep(6)
+    thread2.start()
+
+    thread1.join()
+    thread2.join()
+
+    dataFrame = pd.DataFrame(data=listaNotas, index=[0])
+    dataFrame = (dataFrame.T)
+    print(dataFrame)
+    dataFrame.to_excel('teste.xlsx')
