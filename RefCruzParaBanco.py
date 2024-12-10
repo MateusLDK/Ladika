f66de776761d579667f75b462c8fd5bc4e71199b
MateusLDK <ladikamateus@gmail.com>
2024-12-10 14:24:03 -0300

    processo finalizado

--- RefCruzParaBanco.py
+++ RefCruzParaBanco.py
@@ -4,16 +4,21 @@ from tkinter import filedialog as fd
 folderPath = fd.askopenfilename()
 
 dataFrame = pd.read_excel(folderPath)
+colunasEncontradas = []
 listaLojas=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,201,202,203,204,205,206,207,208,209,210,602,301,302,303,304,305,306,307,308,309,310]
 colunasPlanilha = list(dataFrame.columns)
+print(colunasPlanilha)
 
 for coluna in colunasPlanilha:
 
-    if coluna in str(listaLojas):
-        colunasPlanilha.remove(coluna)
-    
+    if coluna in listaLojas:
+        colunasEncontradas.append(coluna)
+
+for loja in colunasEncontradas:
+    colunasPlanilha.remove(loja)
+
 dataFrameArrumado = dataFrame.melt(
-    id_vars=[colunasPlanilha], var_name="Lojas", value_name="Quantidade")
+    id_vars=colunasPlanilha, value_vars=colunasEncontradas, var_name="Loja", value_name="Minimo")
 
 print(dataFrameArrumado)
-dataFrameArrumado.to_excel(excel_writer="planilha arrumada.xlsx")
+dataFrameArrumado.to_excel(excel_writer="planilha arrumada.xlsx", index = False)
